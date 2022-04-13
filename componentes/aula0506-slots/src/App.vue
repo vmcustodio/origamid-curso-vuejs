48732b57e065030def425c5d001d5882e7164c6f
vmcustodio <van.custodio.vc@gmail.com>
2022-04-13 14:12:46 -0300

    vue v-slots

---
+++ componentes/aula0506-slots/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <modal-principal>
+     <template v-slot:header="{ logo }">
+       Esse é o header
+       {{logo}}
+     </template>
+     <template v-slot:default="{ user }">  <!-- desestruturação de objetos - ira procurar direto no objeto que vem se existe alguma propriedade chamada user  -->
+        <p>{{user.nome}}</p> 
+        <p>Esse é um teste</p>  <!-- aparecerá todo o conteudo que está aqui dentro do slot -->
+        <button>Comprar</button>
+      </template>
+      <template v-slot:footer>
+        <p>Esse é o footer</p> 
+      </template>
+    </modal-principal>
+  </div>
+</template>
+
+<script>
+import ModalPrincipal from "./components/ModalPrincipal.vue"
+export default {
+  name: 'app',
+  components: {
+    ModalPrincipal
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ componentes/aula0506-slots/src/components/ModalPrincipal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="modal">
+    <slot name="header" :logo="logo"></slot> <!-- aqui importa a organização do slot -->
+    <slot :user="user"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ModalPrincipal',
+  data() {
+    return {
+      logo: "Techno",
+      user: {
+        nome: "André"
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
