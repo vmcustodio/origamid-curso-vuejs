d9ed280270e4a5c04544d62ac5d3fc432d779bc8
vmcustodio <van.custodio.vc@gmail.com>
2022-04-14 10:14:08 -0300

    vue router

---
+++ vue-router/aula0702-vuerouter/src/views/CursoAulas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Lista de Aulas</h2>
+    <ul>
+      <li>Aula 1</li>
+      <li>Aula 2</li>
+      <li>Aula 3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ vue-router/aula0702-vuerouter/src/views/CursoDescricao.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Essa é a descrição do curso.</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
